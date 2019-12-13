13315da1bb81e902b12afd9bc64eddf7df9b5fb8
Pahrul Irfan <ip.5090@gmail.com>
2019-12-13 17:48:07 +0800

    add style + sql

--- P6/index.php
+++ P6/index.php
@@ -9,39 +9,40 @@ require 'connect.php';
     <link rel="stylesheet" href="../P4/bootstrap/css/bootstrap.min.css">
 </head>
 <body>
-<h2>Data Skripsi</h2>
-<hr>
-<table class="table table-bordered" border="1" width="70%">
-    <thead>
-    <tr>
-        <th>No</th>
-        <th>NIM</th>
-        <th>Nama</th>
-        <th>Judul</th>
-        <th>Pembimbing</th>
-        <th>Aksi</th>
-    </tr>
-    </thead>
-    <tbody>
-    <?php
-    // membuat query dari PHP
-    $query = mysqli_query($koneksi, "select * from skripsi");
-    $no = 1;
-    while ($data = mysqli_fetch_array($query)) :
-        ?>
+<div class="container">
+    <h2>Data Skripsi</h2>
+    <hr>
+    <table class="table table-bordered" border="1" width="70%">
+        <thead>
         <tr>
-            <td><?= $no++ ?></td>
-            <td><?php echo $data['nim']; ?> </td>
-            <td><?= $data['nama']; ?> </td>
-            <td><?= $data['judul']; ?> </td>
-            <td><?= $data['pembimbing']; ?> </td>
-            <td>Update | Delete</td>
+            <th>No</th>
+            <th>NIM</th>
+            <th>Nama</th>
+            <th>Judul</th>
+            <th>Pembimbing</th>
+            <th>Aksi</th>
         </tr>
-    <?php
-    endwhile;
-    ?>
-    </tbody>
-</table>
-
+        </thead>
+        <tbody>
+        <?php
+        // membuat query dari PHP
+        $query = mysqli_query($koneksi, "select * from skripsi");
+        $no = 1;
+        while ($data = mysqli_fetch_array($query)) :
+            ?>
+            <tr>
+                <td><?= $no++ ?></td>
+                <td><?php echo $data['nim']; ?> </td>
+                <td><?= $data['nama']; ?> </td>
+                <td><?= $data['judul']; ?> </td>
+                <td><?= $data['pembimbing']; ?> </td>
+                <td>Update | Delete</td>
+            </tr>
+        <?php
+        endwhile;
+        ?>
+        </tbody>
+    </table>
+</div>
 </body>
 </html>
