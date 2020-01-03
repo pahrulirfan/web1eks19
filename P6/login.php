62de5c0ee6bdb6bd7f46c45caea509bdf04fce84
Pahrul Irfan <ip.5090@gmail.com>
2020-01-03 17:45:04 +0800

    form login

---
+++ P6/login.php
@@ -0,0 +1,33 @@
+<!doctype html>
+<html>
+<head>
+    <title>Login</title>
+    <link rel="stylesheet" href="../P4/bootstrap/css/bootstrap.min.css">
+</head>
+<body>
+<div class="container">
+    <div class="row">
+        <div class="col-md-4 mt-5">
+            <div class="card">
+                <div class="card-header bg-success">Login</div>
+                <div class="card-body">
+
+                    <form action="" method="post">
+                        <div class="form-group">
+                            <label>Email</label>
+                            <input type="email" name="txtemail" class="form-control">
+                        </div>
+                        <div class="form-group">
+                            <label>Password</label>
+                            <input type="password" name="txtpassword" class="form-control">
+                        </div>
+                        <input type="submit" value="Login" name="login" class="btn btn-primary">
+                    </form>
+
+                </div>
+            </div>
+        </div>
+    </div>
+</div>
+</body>
+</html>
